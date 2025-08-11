e08254cd06f81cba81e3fe1b641ae8e286840140
Tom0Brien <thomas16441@gmail.com>
2025-08-11 20:59:37 +1000

    Add install

---
+++ scripts/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set these variables to match your robot's configuration
+ROBOT_USER="booster"
+ROBOT_IP="192.168.54.102"
+ROBOT_PATH="/home/booster/Workspace/robocup_demo"
+
+# Get the absolute path to the project root (one level up from this script)
+SRC_PATH="$(cd "$(dirname "$0")/.." && pwd)"
+
+# Sync the code to the robot
+rsync -avz --delete \
+  --exclude='.git' \
+  --exclude='*.pyc' \
+  --exclude='CMakeCache.txt' \
+  --exclude='CMakeFiles/' \
+  --exclude='build/' \
+  --exclude='log/' \
+  --exclude='install/' \
+  "${SRC_PATH}/" "${ROBOT_USER}@${ROBOT_IP}:${ROBOT_PATH}/"
